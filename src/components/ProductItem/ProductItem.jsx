0a93bc99aee724deb4b4af1b3c2d8fe6e917e309
Giga <das19901990a@gmail.com>
2026-01-03 11:41:07 +0200

    ffdfd

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from '../Button/Button';
 import './ProductItem.css';
-import image from '../../../public/pexels.jpg';
+
 
 const ProductItem = ({ product, className, onAdd }) => {
     const onAddHandler = () => {
